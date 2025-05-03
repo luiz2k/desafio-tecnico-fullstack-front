eeeb76feeb86780b0aba40c649bc9aa69578c4c4
luiz2k <luizteles934@gmail.com>
2025-05-03 14:22:50 -0300

    feat: adicionar wrapper para as requisições http

---
+++ src/lib/http-client.ts
@@ -0,0 +1,44 @@
+import { cookies } from "next/headers";
+
+type HttpMethod = "GET" | "POST" | "PUT" | "DELETE" | "PATCH";
+
+interface RequestOptions extends Omit<RequestInit, "body"> {
+  method?: HttpMethod;
+  body: Record<string, unknown>;
+  headerAuthorization?: boolean;
+}
+
+type ErrorResponse = {
+  message: string;
+  error: string;
+  status: number;
+};
+
+/**
+ * Função para fazer requisições HTTP para a API.
+ *
+ * Essa função é um wrapper para padronizar as requisições HTTP.
+ */
+export async function http<R>(url: string, init: RequestOptions): Promise<R> {
+  const baseUrl = process.env.API_URL;
+
+  const response = await fetch(`${baseUrl}${url}`, {
+    ...init,
+    body: init.body ? JSON.stringify(init.body) : undefined,
+    headers: {
+      "Content-Type": "application/json",
+      ...init.headers,
+      ...(init.headerAuthorization && {
+        Authorization: `Bearer ${cookies().get("token")?.value}`,
+      }),
+    },
+  });
+
+  if (!response.ok) {
+    const data = (await response.json()) as ErrorResponse;
+
+    throw new Error(data.message);
+  }
+
+  return await response.json();
+}
