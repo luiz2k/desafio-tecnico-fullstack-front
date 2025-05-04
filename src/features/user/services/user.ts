48147d08bb70734dd0320ba43b4202f6b8ce64e7
luiz2k <luizteles934@gmail.com>
2025-05-04 14:27:59 -0300

    feat: criar service para lidar com o crud de user

---
+++ src/features/user/services/user.ts
@@ -0,0 +1,34 @@
+import { http } from "@/lib/http-client";
+import { UserRole } from "@/types/user-role-enum";
+import { CreateUserDto } from "../validations/create-user-validation";
+
+type User = {
+  _id: string;
+  email: string;
+  roles: UserRole[];
+  __v: number;
+};
+
+export const user = {
+  async create(data: CreateUserDto) {
+    return await http<User>("/user", {
+      method: "POST",
+      body: data,
+      headerAuthorization: true,
+    });
+  },
+
+  async findAll() {
+    return await http<User[]>("/user", {
+      method: "GET",
+      headerAuthorization: true,
+    });
+  },
+
+  async delete(id: string) {
+    return await http<User[]>(`/user/${id}`, {
+      method: "DELETE",
+      headerAuthorization: true,
+    });
+  },
+};
