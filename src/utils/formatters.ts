9822b33cdd75fb91c1b09b031b2432867ba1b21a
luiz2k <luizteles934@gmail.com>
2025-05-06 14:27:38 -0300

    docs: adicionar comentário explicando o que cada variáveis faz

--- src/utils/formatters.ts
+++ src/utils/formatters.ts
@@ -1,10 +1,19 @@
+/**
+ * Função para abreviar um número em uma string.
+ * Ex: 1000 -> "1 mil"
+ * Ex: 1000000 -> "1 mi"
+ */
 export const abbreviateNumber = new Intl.NumberFormat("pt-BR", {
-  notation: "compact",
-  compactDisplay: "short",
+  notation: "compact", // Abrevia o número em uma string
+  compactDisplay: "short", // Mostra a abreviação mais curta possível
 });
 
+/**
+ * Função para formatar uma data em uma string.
+ * Ex: new Date(2022, 0, 1) -> "1/1/2022"
+ */
 export const dateFormatter = new Intl.DateTimeFormat("pt-BR", {
-  day: "numeric",
-  month: "numeric",
-  year: "numeric",
+  day: "numeric", // Mostra o dia da semana
+  month: "numeric", // Mostra o mês
+  year: "numeric", // Mostra o ano
 });
