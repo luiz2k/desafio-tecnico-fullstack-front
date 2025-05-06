5227ef82b0cbc5fac21bf247a7f489e5a66a3d72
luiz2k <luizteles934@gmail.com>
2025-05-05 21:26:26 -0300

    feat: adicionar action responsável por criar uma nova campanha

---
+++ src/features/campaign/actions/create-campaign-action/create-campaign-action.ts
@@ -0,0 +1,15 @@
+"use server";
+
+import { revalidatePath } from "next/cache";
+import { campaign } from "../../services/campaign";
+import { CreateCampaignParticipantDto } from "../../validations/create-campaign-validation";
+
+export async function createCampaignAction(data: CreateCampaignParticipantDto) {
+  const response = await campaign.create(data);
+
+  if (response.error) {
+    return response;
+  }
+
+  revalidatePath("/campaign");
+}
