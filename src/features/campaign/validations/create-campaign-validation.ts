1a96c4a05947424cdda1cca6f07de169917cfb55
luiz2k <luizteles934@gmail.com>
2025-05-05 21:21:51 -0300

    feat: melhorias na validação responsável pela criação de campanha
    - Nos campos tipo date usar a validação dateStringSchema
    - Adicionar caracteres mínimos para title e customer
    - Deixar o status como obrigatório

--- src/features/campaign/validations/create-campaign-validation.ts
+++ src/features/campaign/validations/create-campaign-validation.ts
@@ -1,24 +1,26 @@
 import { CampaignStatus } from "@/features/enums/campaign-status-enum";
+import { dateStringSchema } from "@/validations/date-string-validation";
 import { z } from "zod";
 
 export const createCampaignSchema = z.object({
-  title: z.string({
-    required_error: "Título obrigatório",
-    invalid_type_error: "O título precisa ser uma string",
-  }),
-  customer: z.string({
-    required_error: "Cliente obrigatório",
-    invalid_type_error: "O cliente precisa ser uma string",
-  }),
-  createdAt: z.date().optional(),
-  updatedAt: z.date().optional(),
-  finishedAt: z.date().nullable().optional(),
-  status: z
-    .nativeEnum(CampaignStatus, {
-      required_error: "Status obrigatório",
-      invalid_type_error: "O status deve ser um valor válido",
+  title: z
+    .string({
+      required_error: "Título obrigatório",
+      invalid_type_error: "O título precisa ser uma string",
     })
-    .optional(),
+    .min(3, { message: "Título muito curto, mínimo 3 caracteres" }),
+  customer: z
+    .string({
+      required_error: "Cliente obrigatório",
+      invalid_type_error: "O cliente precisa ser uma string",
+    })
+    .min(3, { message: "Cliente muito curto, mínimo 3 caracteres" }),
+  startedAt: dateStringSchema,
+  finishedAt: dateStringSchema,
+  status: z.nativeEnum(CampaignStatus, {
+    required_error: "Status obrigatório",
+    invalid_type_error: "O status deve ser um valor válido",
+  }),
 });
 
 export type CreateCampaignDto = z.infer<typeof createCampaignSchema>;
