3b410dcd56460c730ee1cd7d329f4f6d08c5f9d2
luiz2k <luizteles934@gmail.com>
2025-05-05 21:24:48 -0300

    feat: adicionar formatador para data

--- src/utils/formatters.ts
+++ src/utils/formatters.ts
@@ -2,3 +2,9 @@ export const abbreviateNumber = new Intl.NumberFormat("pt-BR", {
   notation: "compact",
   compactDisplay: "short",
 });
+
+export const dateFormatter = new Intl.DateTimeFormat("pt-BR", {
+  day: "numeric",
+  month: "numeric",
+  year: "numeric",
+});
