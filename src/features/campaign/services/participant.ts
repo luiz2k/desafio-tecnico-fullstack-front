1f6efb1b91c22b379bb88673f9fa41a2a79705b4
luiz2k <luizteles934@gmail.com>
2025-05-09 13:05:43 -0300

    feat: adicionar requisição para relacionar vários participantes simultaneamente a uma campanha

--- src/features/campaign/services/participant.ts
+++ src/features/campaign/services/participant.ts
@@ -2,6 +2,14 @@ import { http } from "@/lib/http-client";
 import { Participant } from "../types/participant-type";
 
 export const participant = {
+  async createMany(campaignId: string, participants: string[]) {
+    return await http<Participant[]>(`/participant/bulk/${campaignId}`, {
+      method: "POST",
+      body: participants,
+      headerAuthorization: true,
+    });
+  },
+
   async findByCampaignId(id: string) {
     return await http<Participant[]>(`/participant/${id}`, {
       method: "GET",
