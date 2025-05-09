cddc6a84b99956a33b51038628292ceb1d273008
luiz2k <luizteles934@gmail.com>
2025-05-09 11:12:08 -0300

    feat: passar a props de type para o componente DateInput

--- src/components/date-input/date-input.tsx
+++ src/components/date-input/date-input.tsx
@@ -1,13 +1,15 @@
 import { forwardRef } from "react";
 import { twMerge } from "tailwind-merge";
 
-type DateInputProps = React.InputHTMLAttributes<HTMLInputElement> & {};
+type DateInputProps = React.InputHTMLAttributes<HTMLInputElement> & {
+  type?: "datetime-local" | "date";
+};
 
 export const DateInput = forwardRef<HTMLInputElement, DateInputProps>(
-  ({ className, ...props }, ref) => {
+  ({ className, type = "datetime-local", ...props }, ref) => {
     return (
       <input
-        type="date"
+        type={type}
         className={twMerge(
           "peer h-[38px] w-full select-none rounded-md border border-surface bg-transparent px-2.5 py-2 text-sm text-black shadow-sm outline-none ring ring-transparent transition-all duration-300 ease-in placeholder:text-foreground/60 hover:border-primary hover:ring-primary/10 focus:border-primary focus:outline-none focus:ring-primary/10 disabled:pointer-events-none disabled:opacity-50 aria-disabled:cursor-not-allowed data-[shape=pill]:rounded-full data-[error=true]:border-error data-[success=true]:border-success data-[icon-placement=end]:pe-9 data-[icon-placement=start]:ps-9 dark:text-white",
           className,
