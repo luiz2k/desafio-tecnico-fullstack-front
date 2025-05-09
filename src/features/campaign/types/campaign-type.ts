e18e7aeca65d5b161c55f3aed400092abe60afe6
luiz2k <luizteles934@gmail.com>
2025-05-09 11:14:30 -0300

    feat: na tipagem de Campaign adicionar  no status o enum CampaignStatus

--- src/features/campaign/types/campaign-type.ts
+++ src/features/campaign/types/campaign-type.ts
@@ -1,9 +1,11 @@
+import { CampaignStatus } from "../enums/campaign-status-enum";
+
 export type Campaign = {
   _id: string;
   title: string;
   customer: string;
   startedAt: string;
   finishedAt: string;
-  status: string;
+  status: CampaignStatus;
   __v: number;
 };
