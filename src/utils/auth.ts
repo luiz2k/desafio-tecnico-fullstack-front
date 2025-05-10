54c353dd93c1012483410eff6bb062acfefc8e75
luiz2k <luizteles934@gmail.com>
2025-05-09 21:17:02 -0300

    docs: documentar funções com jsDocs

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -12,7 +12,13 @@ export type Payload = {
   exp: number;
 };
 
-// Decodifica o token de autenticação
+/**
+ * Função para verificar se o usuário está autenticado.
+ *
+ * Retorna o payload do token de acesso, que contém as informações do usuário.
+ *
+ * @returns Payload do token de acesso ou undefined se não autenticado.
+ */
 export async function auth(): Promise<Payload | undefined> {
   const access_token = cookies().get("access_token")?.value;
 
@@ -22,3 +28,4 @@ export async function auth(): Promise<Payload | undefined> {
 
   return decoded;
 }
+
--- src/utils/sign-out.ts
+++ src/utils/sign-out.ts
@@ -3,7 +3,12 @@
 import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
 
-// Deslogar o usuário autenticado
+/**
+ * Faz o logout do usuário.
+ *
+ * Esta função remove o token de acesso dos cookies e redireciona
+ * o usuário para a página de login.
+ */
 export async function signOut() {
   const access_token = cookies().get("access_token")?.value;
 
